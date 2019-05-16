cef475454bcef1a2843f9c8f791d59d7e061447e
2E-2 <2e-2@s42s06mac.local>
2019-05-16 09:59:14 +0200

    Yea

---
+++ serwer/public_html/delta/views/delta-2.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+   <title>Delta2</title>
+</head>
+<body>
+   <h1>Wynik:</h1>
+   <p> x = <?php echo $x ?></p>
+</body>
+</html>
